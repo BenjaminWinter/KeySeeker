883b5ee4ec5c55ccbc0067af1ff3cca1fa2614b2
Marvin Erdmann <marvin-erdmann@gmx.de>
2014-05-20 21:03:35 +0200

    added some files that were missing in the maven prototype.

---
+++ prototypes/maven-gwt-prototype/target/generated-sources/gwt/de/bht/swp/client/Messages.java
@@ -0,0 +1,26 @@
+package de.bht.swp.client;
+
+/**
+ * Interface to represent the messages contained in resource bundle:
+ * 	C:/Users/Marvin/Documents/Eclipse/project/SoftwareProjekt/softwareprojekt-i/prototypes/maven-gwt-prototype/src/main/resources/de/bht/swp/client/Messages.properties'.
+ */
+public interface Messages extends com.google.gwt.i18n.client.Messages {
+  
+  /**
+   * Translated "Enter your name".
+   * 
+   * @return translated "Enter your name"
+   */
+  @DefaultMessage("Enter your name")
+  @Key("nameField")
+  String nameField();
+
+  /**
+   * Translated "Send".
+   * 
+   * @return translated "Send"
+   */
+  @DefaultMessage("Send")
+  @Key("sendButton")
+  String sendButton();
+}
