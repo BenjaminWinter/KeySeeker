3eecfadd70265a49680994eadb902d4df62a910f
Benjamin Winter <Benny.Winter@yahoo.de>
2014-06-30 11:30:57 +0200

    implement business logic for account

--- prototypes/ui-prototype/src/main/java/de/bht/swp/ui_prototype/server/hibernate/DAO/GenericDAOImpl.java
+++ prototypes/ui-prototype/src/main/java/de/bht/swp/ui_prototype/server/hibernate/DAO/GenericDAOImpl.java
@@ -12,51 +12,50 @@ import de.bht.swp.ui_prototype.server.hibernate.Util.TransactionWrapper;
 public class GenericDAOImpl<T> implements GenericDAO<T> {
 
 	private Class<T> type;
-	
+
 	private TransactionWrapper tw = new TransactionWrapper();
-	
+
 	@SuppressWarnings("unchecked")
 	public GenericDAOImpl() {
-        Type t = getClass().getGenericSuperclass();
-        ParameterizedType pt = (ParameterizedType) t;
-        type = (Class<T>) pt.getActualTypeArguments()[0];
-    }
+		Type t = getClass().getGenericSuperclass();
+		ParameterizedType pt = (ParameterizedType) t;
+		type = (Class<T>) pt.getActualTypeArguments()[0];
+	}
+
 	public void saveOrUpdate(final T Object) {
-		Action transaction = new Action(){
+
+		tw.run(new Action() {
 
 			@Override
-			public java.lang.Object execute(Session session) {
+			public Object execute(Session session) {
 				session.saveOrUpdate(Object);
 				return null;
 			}
-			
-		};
-		tw.run(transaction);
+
+		});
 	}
 
 	@SuppressWarnings("unchecked")
 	public T get(final long Id) {
-		Action transaction = new Action(){
+		return (T) tw.run(new Action() {
 
 			@Override
 			public Object execute(Session session) {
 				return session.get(type, Id);
 			}
-		};
-		return (T) tw.run(transaction);
+		});
 	}
 
 	public void delete(final T Object) {
-		Action transaction = new Action(){
+		tw.run(new Action() {
 
 			@Override
-			public java.lang.Object execute(Session session) {
+			public Object execute(Session session) {
 				session.delete(Object);
 				return null;
 			}
-			
-		};
-		tw.run(transaction);
+
+		});
 	}
 
 }
--- prototypes/ui-prototype/src/main/java/de/bht/swp/ui_prototype/server/hibernate/Service/AccountService.java
+++ prototypes/ui-prototype/src/main/java/de/bht/swp/ui_prototype/server/hibernate/Service/AccountService.java
@@ -1,5 +1,9 @@
 package de.bht.swp.ui_prototype.server.hibernate.Service;
 
+import java.net.InetAddress;
+
+import org.apache.tools.ant.taskdefs.email.EmailAddress;
+
 import de.bht.swp.ui_prototype.client.DBObject.Account;
 import de.bht.swp.ui_prototype.server.hibernate.DAO.AccountDAO;
 import de.bht.swp.ui_prototype.server.hibernate.Util.DAOFactory;
@@ -18,7 +22,21 @@ public class AccountService {
 	 *            account to be persisted
 	 */
 	public void saveOrUpdateAccount(Account account) {
-		accountDAO.saveOrUpdate(account);
+		if(isValidEmail(account.getEmail())){
+			if(isValidAccName(account.getAccountName())){
+				if(isValidPassword(account.getPassword())){
+					accountDAO.saveOrUpdate(account);
+				} else {
+					throw new IllegalArgumentException("Password invalid");
+				}
+				
+			} else {
+				throw new IllegalArgumentException("AccountName is invalid");
+			}
+		} else {
+			throw new IllegalArgumentException("Email is Invalid");
+		}
+		
 	}
 
 	/**
@@ -40,5 +58,15 @@ public class AccountService {
 	 */
 	public void deleteAccount(Account account) {
 		accountDAO.delete(account);
+	}
+	private static boolean isValidEmail(String emailAddress) {
+	    return emailAddress.contains(" ") == false && emailAddress.matches(".+@.+\\.[a-z]+");
+	}
+	private static boolean isValidAccName(String accname){
+		return accname.contains(" ") == false && accname.length() > 2 && accname.length() < 12;
+	}
+	
+	private static boolean isValidPassword(String password){
+		return password.length() > 0;
 	}
 }
